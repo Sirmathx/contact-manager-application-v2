27bcb3efbd8adf372dc99d50991e90f0fe848b19
Sirmathx <egary497@gmail.com>
2023-03-24 12:15:07 +0100

    created signup,notfound  and home component

---
+++ client/src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./Home.css";
+import Header from './partials/Header/Header';
+import Footer from './partials/Footer/Footer';
+
+const Home = () => {
+  return (
+   <div>
+    <Header />
+    <h1>home page</h1>
+    <Footer/>
+    </div>
+  );
+  
+};
+
+export default Home;
