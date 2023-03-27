9556b512622acc437f1833045d442c236227c110
Sirmathx <egary497@gmail.com>
2023-03-27 12:52:15 +0100

    built navbar and footer

--- client/src/components/partials/Footer/Footer.js
+++ client/src/components/partials/Footer/Footer.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import {FaUserShield,FaUserPlus, } from "react-icons/fa";
 import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div>
-        <h1>Footer</h1>
-    </div>
+    <footer className='main-footer-wrapper'>
+         <div className='link-grp'>
+            <Link to="/">home</Link>
+            <Link to="/about">about</Link>
+            <Link to="/service">service</Link>
+            <Link to="/contact">contact</Link>
+            <Link className='nav-login-btn' to="/login">
+              login
+              <FaUserShield/>
+            </Link>
+            <Link className='nav-signup-btn' to="/signup">
+              sign up
+              <FaUserPlus/>
+              </Link>
+          </div>
+          <div className='copyright-box'>
+            &copy; copyright 2023 CMA, All right reserved
+          </div>
+    </footer>
   );
 };
 
--- client/src/components/partials/Header/Header.js
+++ client/src/components/partials/Header/Header.js
@@ -1,11 +1,41 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import "./Header.css";
+// import headstyle from "./Header.Modules.css";
+import logo from "../../../assets/CMA-v2.svg";
+import {FaBars,FaUserShield,FaUserPlus, } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <div>
-        <h1>Header</h1>
-    </div>
+    <nav className='main-navbar'>
+        <div className='logo-box'>
+          <Link to="/">
+          <img src={logo} alt="CMA Logo"/>
+
+          </Link>
+        </div>
+        <div className='nav-link-grp'>
+          <div className='nav-toggler'>
+            <span id='toggleBtn'>
+              <FaBars/>
+            </span>
+          </div>
+          <div className='link-grp'>
+            <Link to="/">home</Link>
+            <Link to="/about">about</Link>
+            <Link to="/service">service</Link>
+            <Link to="/contact">contact</Link>
+            <Link className='nav-login-btn' to="/login">
+              login
+              <FaUserShield/>
+            </Link>
+            <Link className='nav-signup-btn' to="/signup">
+              sign up
+              <FaUserPlus/>
+              </Link>
+          </div>
+        </div>
+    </nav>
   );
 };
 
