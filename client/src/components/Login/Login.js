40f73249e1722e7934fa8767210d4701304c322d
Sirmathx <egary497@gmail.com>
2023-03-28 12:14:23 +0100

    create signup and login component

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,48 +1,40 @@
-import React from 'react';
-import "./SignUp.css";
-import Header from '../partials/Header/Header';
-import Footer from "../partials/Footer/Footer";
+import React from 'react'
+import "./Login.css"
+import Footer from '../partials/Footer/Footer';
 import { Link } from "react-router-dom";
-
-const SignUp = () => {
+import Header from '../partials/Header/Header';
+const login = () => {
   return (
     <React.Fragment>
-        <Header/>
-      {/* start of signUP wrapper */}
-      <div className="signup-wrapper">
+       <Header/>
+        <div className="signup-wrapper">
         <form action="" method="post">
           <div className="signup-form">
-            <div className="signup-form-row  form-title">SignUp</div>
+            <div className="signup-form-row  form-title">Login</div>
             
-            <div className="signup-form-row">
-              <input type="text" placeholder="First name" />
-              <input type="text" placeholder="Last name" />
-            </div>
-
             <div className="signup-form-row">
               <input type="email" placeholder="example@emal.com" />
             </div>
 
             <div className="signup-form-row">
               <input type="password" placeholder="password" />
-              <input type="password" placeholder="confirm password" />
             </div>
 
-            <div className="signup-form-row">
+            {/* <div className="signup-form-row">
               <label For="termsCondition">
                 <input type="checkbox" id="termsCondition" />I agree to the
                 <Link to="/termsCondition"> Terms and Conditions</Link>
               </label>
-            </div>
+            </div> */}
 
             <div className="signup-form-row">
-              <button>Create Account</button>
+              <button>Login</button>
             </div>
 
             <div className="signup-form-row">
               <i>
-              Already have an account? &nbsp;
-              <Link to="/login">Login here</Link>
+              don't have an account? &nbsp;
+              <Link to="/login">create an account</Link>
 
               </i>
             </div>
@@ -50,10 +42,9 @@ const SignUp = () => {
           </div>
         </form>
       </div>
-      {/* end of signUP wrapper*/}
         <Footer/>
     </React.Fragment>
-  );
-};
+  )
+}
 
-export default SignUp;
+export default login
